5d593636940edc96f253baa502e83c1fb8e2a378
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-04 14:20:15 -0500

    change url

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module WireframeBackend
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000', 'https://desolate-waters-78828.herokuapp.com/' #replace this url with that of your own heroku client app
+        origins 'http://localhost:3000', 'https://arcane-journey-88268.herokuapp.com/' #replace this url with that of your own heroku client app
         resource '*', :headers => :any, :methods => [:get]
       end
     end
